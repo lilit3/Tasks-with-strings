55462ac444526d6d7b0d72e8386e68c16d8b24d7
lilit3 <43044509+lilit3@users.noreply.github.com>
2018-09-11 13:17:48 +0300

    getStrPos() - done , ubstrCount() - done

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
-
 class ForStringClass
 {
-
     public $longString;
     public $lengthString;
 
-    //public $shotString;
-    //public $lenghtShot;
-
     public function __construct(string $longString){
-	
-	$this->longString = $longString;
-	$this->getStrLen();
+    
+    $this->longString = $longString;
+    $this->getStrLen();
     }
 
     public function getStrLen()
@@ -28,61 +23,64 @@ class ForStringClass
         }
         
         $strToArray = str_split($this->longString);
-
         for ($i = 0; $i < $this->lengthString; $i++) {
-        	if (($i < $start) || ($i >= $len)) {
-        		unset($strToArray[$i]);
+            if (($i < $start) || ($i >= $len)) {
+                unset($strToArray[$i]);
             }
         }
-
         return implode('', $strToArray);
     }
 
     #explode — Разбивает строку с помощью разделителя. Возвращает массив строк, полученных разбиением строки string с использованием delimiter в качестве разделителя.
     public function explodeStr($delimiter)
     {
-
         $newArr = [];
+        $temp = $this->longString;
 
         while ($j = strpos($this->longString, $delimiter)) {
-        	$newArr[] = substr($this->longString, 0, $j); 
-        	$this->longString = substr($this->longString, $j+1);
+            $newArr[] = substr($this->longString, 0, $j); 
+            $this->longString = substr($this->longString, $j+1);
         }
-
         $newArr[] = substr($this->longString, 0); 
+
+        $this->longString = $temp;
        
         return($newArr);
     }
 
    // strpos просто сравнивать по символьно. искать первый симфол и если нашелся то и остальные проверять. если остальных нет то снова искать первый
-    public function getStrPos()
+    public function getStrPos($sub)
     {
+        for ($i = 0; $i < $this->lengthString; $i++) {
+         if (substr_compare($this->longString, $sub, $i, strlen($sub)) == 0) {
+                
+             return $i;
+            }       
+        }
 
-        // for ($i = 0; $i < $this->lengthString; $i++) {
-        // 	if (substr_compare($this->longString, $this->shotString, $i, $this->lenghtShot) == 0) {
-        		
-        // 		return $i;
-        //     }       
-        // }
-        
+        return false;  
     }
 
     #3 substr_count — Возвращает число вхождений подстроки
-    public function substrCount($delimiter)
+    public function substrCount($sub)
     {
+        $substr_count = $this->lengthString - $this->lengthString;
+        $string = $this->longString;
 
-        // while ($x = strpos($this->longString, 's')) {
-        // 	$long = substr($long, ++$x);
-        // 	$substr_count++;
+         while ($x = strpos($string, $sub)) {
+             $string = substr($string, ++$x);
+             $substr_count++;
+        }
 
-        // }
-
-        // return $substr_count;
+        return $substr_count;
     }
-
 }
-
 
 $myString = new ForStringClass('PHP 5 is very very flexible in accessing member variables and member functions.');
 echo $myString->getSubStr(5, 25);
 var_dump($myString->explodeStr(' '));
+
+var_dump($myString->getStrPos('flexible'));
+
+var_dump($myString->substrCount('very'));
+
